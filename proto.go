740ca6fe7bcea067275402f84da82f06a7ba2091
Pierre Thierry <pierre@nothos.net>
2016-12-30 00:42:34 +0100

    Add human readable byte size

--- proto.go
+++ proto.go
@@ -1,10 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
+
+
+func HumanReadableBytes(bytes float64) string {
+	var magnitude = math.Log2(bytes) / 10
+	var power float64
+	var suffix string
+
+	if magnitude > 3 {
+		power = 3
+		suffix = "Gb"
+	} else if magnitude > 2 {
+		power = 2
+		suffix = "Mb"
+	} else if magnitude > 1 {
+		power = 1
+		suffix = "kb"
+	} else {
+		power = 0
+		suffix = "b"
+	}
+
+	if power == 0 {
+		return fmt.Sprintf("%.0f %s", (bytes/math.Pow(1024, power)), suffix)
+	} else {
+		return fmt.Sprintf("%.2f %s", (bytes/math.Pow(1024, power)), suffix)
+	}
+}
+
 
 func StaticFileHandler(name string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
---
+++ proto_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math"
+	"testing"
+)
+
+func TestHumanReadable(t *testing.T) {
+	assert.Equal(t, "1.15 Gb", HumanReadableBytes(math.Pow(1024, 3) * 1.15))
+	assert.Equal(t, "3.26 Mb", HumanReadableBytes(math.Pow(1024, 2) * 3.26))
+	assert.Equal(t, "7.64 kb", HumanReadableBytes(1024 * 7.64))
+	assert.Equal(t, "823 b", HumanReadableBytes(823))
+}
